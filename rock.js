a88ff1359c343f4a39e9977baa6a4f804a8afdfc
Sarabeth Jaffe <jaffes2@rpi.edu>
2015-11-18 01:01:44 -0500

    Custom logging in to keep track of user points. Still trying to collapse template after user is registered.

--- rock.js
+++ rock.js
@@ -1,6 +1,9 @@
 if (Meteor.isClient) {
+
   // counter starts at 0
   Session.setDefault('counter', 'none');
+  Session.setDefault('needsToRegister', true);
+  Session.setDefault('email', 'email');
 
   Template.submit.helpers({
     counter: function () {
@@ -8,13 +11,52 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.navigation.helpers({
+  	email: function(){
+  		return Session.get('email');
+  	}
+  });
+
+  Template.register.helpers({
+  	needsToRegister: function(){
+  		Session.set('needsToRegister', false);
+  		return Session.get('needsToRegister');
+  	}
+  })
+
+ /*Template.register.needsToRegister = function(){
+ 	return Session.get("needsToRegister");
+ }*/
+
   Template.submit.events({
     'click button': function () {
+
+    	 console.log(Meteor.users.find().fetch());
+    	 console.log(Meteor.user());
       // increment the counter when button is clicked
-      //Session.set('counter', 'clear');   
+        alert(Session.get('email'));   
     }
   });
     
+  Template.register.events({
+    'click Register': function () {
+      // increment the counter when button is clicked
+      alert("registered");
+      Session.set('needsToRegister', false);
+    }
+  });
+
+  Template.navigation.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+
+        Session.set('needsToRegister', true);
+        alert(Session.get('needsToRegister'));
+        //Router.go('#');
+    }
+  });
+
   Template.rock.events({
     'click button': function () {
       // increment the counter when button is clicked
@@ -35,10 +77,33 @@ if (Meteor.isClient) {
       Session.set('counter', 'scissors');   
     }
   });
+
+ Template.register.events({
+   'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        var points = 0;
+        Session.set('email', email);
+
+        Accounts.createUser({
+            email: email,
+            password: password,
+            points: points
+        });
+
+        Session.set('needsToRegister', false);
+     	alert(Session.get('needsToRegister'));
+	}
+
+});
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
 }
+
