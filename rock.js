a0bb7232b9afbecfc2c3575bb052c18af8aeb267
Sarabeth Jaffe <jaffes2@rpi.edu>
2015-11-17 22:26:41 -0500

    Submit button added, onclik event handlers

--- rock.js
+++ rock.js
@@ -2,17 +2,37 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 'none');
 
-  Template.hello.helpers({
+  Template.submit.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
-  Template.hello.events({
+  Template.submit.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', 'selected');
-      
+      //Session.set('counter', 'clear');   
+    }
+  });
+    
+  Template.rock.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', 'rock');   
+    }
+  });
+
+   Template.paper.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', 'paper');   
+    }
+  });
+
+    Template.scissors.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', 'scissors');   
     }
   });
 }
